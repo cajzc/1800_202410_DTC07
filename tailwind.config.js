799f287e09d6a42b96ea5b34f6e1d2898b6afd8e
Dylan Reimer <reimer.dylan@gmail.com>
2024-02-14 10:45:07 -0800

    Add tailwind.config.js to match html script

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0000ff',
+        secondary: '#548c05',
+        background: '#75c4f9',
+        accent: '#f2ab29'
+      }
+    }
+  }
+}

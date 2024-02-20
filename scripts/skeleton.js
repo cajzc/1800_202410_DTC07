a39ef06045537f8804011cb6a4974969e10b9b97
Dylan Reimer <reimer.dylan@gmail.com>
2024-02-20 12:29:03 -0800

    Put navbar and header in separate files and link to index

---
+++ scripts/skeleton.js
@@ -0,0 +1,23 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton() {
+    //CHANGE AFTER FIREBASE WORKS
+    console.log($('#navbarPlaceholder').load('./pages/nav_after_login.html'));
+    console.log($('#footerPlaceholder').load('./pages/footer.html'));
+
+    /*firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#navbarPlaceholder').load('./pages/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('./pages/footer.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#navbarPlaceholder').load('./pages/nav_before_login.html'));
+            console.log($('#footerPlaceholder').load('./pages/footer.html'));
+        }
+    });*/
+}
+loadSkeleton(); //invoke the function

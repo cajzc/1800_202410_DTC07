62c209134692d472bbb38599343595e2201a6f97
Cainan Ziemski Cho <cainan@macpost.com>
2024-02-23 16:36:13 -0800

    Add firebase script configs

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+function getNameFromAuth() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log('User is logged in!')
+            console.log(user.displayName)
+
+            document.getElementById('name-goes-here').innerHTML = user.displayName
+
+        } else {
+            console.log("User is NOT LOGGED IN!")
+
+        }
+
+
+    })
+}
+
+getNameFromAuth()

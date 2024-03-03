34c8d22202b37162dd7b33da2f838405a3512c10
cz <117609173+cajzc@users.noreply.github.com>
2024-03-02 17:05:40 -0800

    Fix js file import syntax

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -15,22 +15,14 @@ var uiConfig = {
             document.getElementById('loader').style.display = 'none';
         }
     },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: 'main.html',
+    signInFlow: 'popup', // Use popup for sign-in flow
+    signInSuccessUrl: '../pages/main.html', // Redirect URL after successful sign-in
     signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        firebase.auth.EmailAuthProvider.PROVIDER_ID, // Allow sign-in with email/password
     ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
+    tosUrl: '<your-tos-url>', // Terms of service URL
+    privacyPolicyUrl: '<your-privacy-policy-url>' // Privacy policy URL
 };
+
 
 ui.start('#firebaseui-auth-container', uiConfig);

8f4793b7d1a3f98eabfc943ed32c21c293a0bde7
Dylan Reimer <reimer.dylan@gmail.com>
2024-03-12 10:24:09 -0700

    Update navbars for before/after login. Redirect home buttons to main. Add logout button and profile button to hamburger

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,20 +3,18 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton() {
-    
+
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
             // User is signed in.
             // Do something for the user here.
-            console.log($('#navbarPlaceholder').load('./pages/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./pages/footer.html'));
-            console.log($('#reviewBox').load('./pages/review_box_templete.html'));
+            console.log($('#navbarPlaceholder').load('../pages/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('../pages/footer.html'));
+            console.log($('#reviewBox').load('../pages/review_box_templete.html'));
         } else {
             // No user is signed in.
             console.log($('#navbarPlaceholder').load('../pages/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('../pages/footer.html'));
-            console.log($('#reviewBox').load('./pages/review_box_templete.html'));
-
+            console.log($('#reviewBox').load('../pages/review_box_templete.html'));
         }
     });
 }

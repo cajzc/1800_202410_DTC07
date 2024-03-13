c137fc2b43169c5032f72f98272b66b546f575ae
Dylan Reimer <reimer.dylan@gmail.com>
2024-03-13 13:46:43 -0700

    Add pause functionality

--- scripts/commute.js
+++ scripts/commute.js
@@ -4,7 +4,6 @@ function setup() {
     $("#transferOptions").hide()
 
     let currentCommute = localStorage.getItem("currentCommute")
-    console.log(currentCommute)
 
     if (currentCommute != null && currentCommute != "started") {
         $("#currentCommuteIcon").attr("name", `${currentCommute}`)
@@ -14,11 +13,9 @@ function setup() {
     if ($("#currentCommuteIcon").attr("name") == "play") {
         $("#transferButton").hide()
         $("#transferOptions").show()
-        console.log("commute hasnt started")
     }
 
     $("#transferButton").on("click", () => {
-        console.log("trasnfer clicked")
         $("#transferButton").hide()
         $("#transferOptions").show()
     })
@@ -26,7 +23,7 @@ function setup() {
     // $("#transferSelect").on("click", () => { selectTransfer() })
 
     $("#stopButton").on("click", () => {
-        localStorage.removeItem("currentCommute")
+        localStorage.clear()
         $("#currentCommuteIcon").attr("name", "play")
         $("#currentCommuteIcon").text("play_circle")
     })
--- scripts/handleCommutes.js
+++ scripts/handleCommutes.js
@@ -1,50 +1,121 @@
 
-function startCommute() {
+function startCommute(user) {
     // Check if the user is logged in:
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            commuteID = db.collection("users").doc(user.uid).collection("commutes").doc().id
-            var currentUserCommute = db.collection("users").doc(user.uid).collection("commutes");
-            currentUserCommute.doc(commuteID).set({
-                name: firebase.firestore.Timestamp.now().toDate()
-            });
 
-            legID = currentUserCommute.doc(commuteID).collection("commuteLegs").doc().id
+    commuteID = db.collection("users").doc(user.uid).collection("commutes").doc().id
+    var currentUserCommute = db.collection("users").doc(user.uid).collection("commutes");
+    currentUserCommute.doc(commuteID).set({
+        name: firebase.firestore.Timestamp.now().toDate()
+    });
 
-            currentUserCommute.doc(commuteID).collection("commuteLegs").doc(legID).set({
-                startTime: firebase.firestore.Timestamp.now(),
-                endTime: firebase.firestore.Timestamp.now(),
-                pauseStartTime: 0,
-                pauseEndTime: 0,
-                cost: 0,
-                method: "walk",
-                /*startLocation: navigator.geolocation.getCurrentPosition(),
-                endLocation: navigator.geolocation.getCurrentPosition()*/
+    startLeg(user, commuteID)
+
+    localStorage.setItem("currentCommuteID", commuteID)
 
 
-            })
-
-            localStorage.setItem("currentCommuteID", commuteID)
-            localStorage.setItem("currentLegID", legID)
-
-        } else {
-            console.log("No user is logged in."); // Log a message when no user is logged in
-        }
-    })
+    console.log("commute Started")
 }
 
-function setTransit() {
+function startLeg(user, commuteID) {
+    let legID = localStorage.getItem("currentLegID")
+    let legCount = 1
+    if (legID != null) {
+        legCount = parseInt(legID.slice(3)) + 1
+        legID = `leg${legCount}`
+    } else {
+        legID = "leg1"
+    }
+
+    db.collection("users").doc(user.uid).collection("commutes").doc(commuteID).collection("commuteLegs").doc(legID).set({
+        startTime: firebase.firestore.Timestamp.now(),
+        endTime: firebase.firestore.Timestamp.now(),
+        cost: 0,
+        method: "walk",
+        leg: legCount,
+        /*startLocation: navigator.geolocation.getCurrentPosition(),
+        endLocation: navigator.geolocation.getCurrentPosition()*/
+    })
+
+    localStorage.setItem("currentLegID", legID)
+
+    console.log(legID + " started")
+
+}
+
+function setTransit(user) {
     let transitType = localStorage.getItem("currentCommute")
     let commuteId = localStorage.getItem("currentCommuteID")
     let legId = localStorage.getItem("currentLegID")
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            db.collection("users").doc(user.uid).collection("commutes").doc(commuteId).collection("commuteLegs").doc(legId).update({
-                method: transitType
-            })
-        } else {
-            console.log("No user is logged in."); // Log a message when no user is logged in
-        }
+
+    db.collection("users").doc(user.uid).collection("commutes").doc(commuteId).collection("commuteLegs").doc(legId).update({
+        method: transitType
+    })
+
+    console.log("transit set to " + transitType)
+
+}
+
+function togglePause(user) {
+
+    let pauseID = localStorage.getItem("pauseID")
+    let paused = localStorage.getItem("paused")
+    let commuteId = localStorage.getItem("currentCommuteID")
+    let legId = localStorage.getItem("currentLegID")
+
+    if (pauseID == null)
+        pauseID = "pause1"
+
+    if (paused == null) {
+        $("#pauseButton").text("play_circle")
+        $("#commuteText").text("Your Commute is Paused!")
+        localStorage.setItem("paused", 1)
+
+        db.collection("users").doc(user.uid).collection("commutes").doc(commuteId).collection("commuteLegs").doc(legId).collection("pauses").doc(pauseID).set({
+            pauseStartTime: firebase.firestore.Timestamp.now(),
+            pauseEndTime: firebase.firestore.Timestamp.now()
+        })
+
+        localStorage.setItem("pauseID", pauseID)
+    } else {
+        localStorage.removeItem("paused")
+        $("#pauseButton").text("pause_circle")
+        writeTime(user)
+
+        db.collection("users").doc(user.uid).collection("commutes").doc(commuteId).collection("commuteLegs").doc(legId).collection("pauses").doc(pauseID).update({
+            pauseEndTime: firebase.firestore.Timestamp.now()
+        })
+
+        pauseCount = parseInt(pauseID.slice(5)) + 1
+        pauseID = `pause${pauseCount}`
+        localStorage.setItem("pauseID", pauseID)
+
+    }
+}
+
+function writeTime(user) {
+    let commuteId = localStorage.getItem("currentCommuteID")
+
+    let currentTime = (firebase.firestore.Timestamp.now().toMillis()) / 1000
+
+    getStartTime(user, commuteId)
+    let commuteStartTime = localStorage.getItem("startTime")
+    let totalTime = currentTime - commuteStartTime
+    let hours = Math.floor(totalTime / 3600)
+    let minutes = Math.floor((totalTime - hours * 3600) / 60)
+    let seconds = Math.floor((totalTime - hours * 3600) % 60)
+
+    $("#commuteText").html(` 
+    You're on the way!<br>Current Time:<br>
+    ${hours} hrs, ${minutes} mins, ${seconds} secs
+    `)
+
+}
+
+function getStartTime(user, commuteId) {
+    commuteLeg = db.collection("users").doc(user.uid).collection("commutes").doc(commuteId).collection("commuteLegs").doc(("leg1"))
+    commuteLeg.get().then(firstLeg => {
+        let startingTime = firstLeg.data().startTime.toMillis() / 1000
+        localStorage.setItem("startTime", startingTime)
     })
 }
 
@@ -86,18 +157,50 @@ function selectTransfer() {
 
 function setup() {
     let currentCommute = localStorage.getItem("currentCommute")
-    console.log(currentCommute)
 
-    if (currentCommute == null) {
-        startCommute()
-        localStorage.setItem("currentCommute", "started")
-        console.log("weve gone")
-    }
+    let paused = localStorage.getItem("paused")
+    console.log(paused)
 
-    $("#transferSelect").on("click", () => {
-        selectTransfer()
-        setTransit()
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+
+            console.log(currentCommute)
+
+            if (currentCommute == null) {
+                startCommute(user)
+                localStorage.setItem("currentCommute", "started")
+            }
+
+            $("#transferSelect").on("click", () => {
+                selectTransfer()
+                setTransit(user)
+            })
+
+
+            if (paused != null) {
+                $("#pauseButton").text("play_circle")
+                $("#commuteText").text("Your Commute is Paused!")
+            }
+
+            $("#pauseButton").on("click", () => {
+                togglePause(user)
+            })
+
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
     })
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
